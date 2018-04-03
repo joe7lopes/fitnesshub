9295280fcc3c848daaf8a046946f69bbdb4feb21
Joao Oliveira <j.andre.oliveira@gmail.com>
2018-04-03 12:20:59 +0200

    initial project scarfolding

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if(process.env.NODE_ENV === 'test-dev' || process.env.NODE_ENV === 'development'){
+  require('dotenv').config({ path: '.env.test'});
+}
+
+module.exports = (env, argv) => {
+
+  const isProduction = env === 'production';
+  const cssExtract = new ExtractTextPlugin('styles.css');
+
+return {
+    entry: ["babel-polyfill", './src/app.js'],
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: (/\.s?css$/),
+        use: cssExtract.extract({
+          use: [
+            // 'style-loader',
+            {loader: 'css-loader', options:{sourceMap: true}},
+            {loader: 'sass-loader', options:{sourceMap: true}}
+          ]
+        })
+      },{
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+        options:{
+          name: 'fonts/[name].[ext]'
+        }
+      }]
+    },
+    plugins: [
+      cssExtract,
+      new webpack.DefinePlugin({
+        'FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'ENV': JSON.stringify(process.env.NODE_ENV)
+      })
+    ],
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/'
+    }
+  };
+}
